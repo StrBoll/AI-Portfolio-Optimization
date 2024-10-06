82a32c885f3300fd0ee8aac92deeb6117083e792
StrBoll <phillipboll107@gmail.com>
2024-10-06 16:38:51 -0400

    cpp setup

---
+++ src/cpp/PortfolioManager.cpp
@@ -0,0 +1,28 @@
+#include "PortfolioManager.h"
+
+
+void PortfolioManager::initPortfoliio() {
+
+    // initializes our portfolio and injects user asset allocation and preferences from sql database into it 
+
+    // will load user specific data that they enter, including initial investment, risk tolerance preferneces and the type of investments that they wanna make 
+}
+
+void PortfolioManager::optimizePortfolio() {
+    
+    // will essentially send the market data and user preferences to the trained model in python
+
+    // AI model will work its magic 
+    
+    // Retrieve that results through here and then output it 
+}
+
+void PortfolioManager::balanceAgain() {
+
+    // Since the market is always changing, this function is gonna be used to rebalance the portfolio
+
+    // Will suggest whether a rebalance is needed, and provide options for rebalancing 
+
+    // Our job is not to do it for you, we simply make the suggestions and its up to the user to decide what they're gonna do with the information provided to them 
+    
+}
---
+++ src/cpp/Simulation.cpp
@@ -0,0 +1,19 @@
+#include "Simulation.h"
+
+// Going to use the monte carlo simulations, in order to optimize this portfolio
+
+
+void Simulation::MonteSimulateStart() {
+    // starts the portfolio performance simulation using the Monte Carlo Method 
+
+    // Run simulation for the different market scenarios to assess risk to reward ratio
+
+}
+
+void Simulation::evaluateRisk() {
+
+    // used hand in hand with portfolio management optimization function 
+
+    
+
+}
