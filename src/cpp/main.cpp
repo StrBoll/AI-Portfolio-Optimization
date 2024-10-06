cc17d52ff2f6d3be56ac0c6c10af7370e7a7c47c
StrBoll <phillipboll107@gmail.com>
2024-10-06 16:29:02 -0400

    main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -8,6 +8,18 @@ int main() {
     //landing zone for testing in cpp
 
 
+    // Connect to our database
+
+
+    // Load our data from market
+
+    // Set up PortfolioManager object for managing asset allocations/suggestions
+
+    // Run the optimization against our trained model
+
+    // Return results
+
+
     return 0;
 }
 
