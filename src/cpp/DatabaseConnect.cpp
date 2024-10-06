92e736005f6e6a881b3808553c4d34d8e9ca26a3
StrBoll <phillipboll107@gmail.com>
2024-10-06 16:32:34 -0400

    bones databaseconnection

---
+++ src/cpp/DatabaseConnect.cpp
@@ -0,0 +1,25 @@
+#include "DatabaseConnect.h"
+#include <pqxx/pqxx>
+
+
+using namespace pqxx;
+
+// Use PQXX to connect to our database and get some functions going 
+
+
+
+void DatabaseConnect::getPreferences() {
+
+    // accessses db.sql and gets users preferences
+
+}
+
+
+void DatabaseCOnnect::getMarket() {
+
+    // accesses market data from psql database table
+
+
+}
+
+
